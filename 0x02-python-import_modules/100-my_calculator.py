6a5ed5c27074e0a0d616b4143965e1781f1a7c8f
KEvans254 <evanstoja@gmail.com>
2022-06-02 22:11:19 +0300

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-    arguments = argv[1:]
-    a = int(arguments[0])
-    o = arguments[1]
-    b = int(arguments[2])
-    i = len(arguments)
-    operator = ["+", "-", "*", "/"]
-    if o not in operator:
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if (__name__ == "__main__"):
+    argc = len(argv)
+    result = 0
+    operators = ["+", "-", "*", "/"]
+
+    if (argc != 4):
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+
+    if (argv[2] not in operators):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    num1 = int(argv[1])
+    operator = argv[2]
+    num2 = int(argv[3])
+
+    if (operator == "+"):
+        result = add(num1, num2)
+    elif (operator == "-"):
+        result = sub(num1, num2)
+    elif (operator == "*"):
+        result = mul(num1, num2)
     else:
-        if o == operator[0]:
-            result = add(a, b)
-        elif o == operator[1]:
-            result = sub(a, b)
-        elif o == operator[2]:
-            result = mul(a, b)
-        else:
-            result = div(a, b)
-        print("{:d} {:s} {:d} = {:d}".format(a, o, b, result))
-    if i != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        result = div(num1, num2)
+    print("{:d} {:s} {:d} = {:d}".format(num1, operator, num2, result))
