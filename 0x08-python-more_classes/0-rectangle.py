9f3fbeeaf6cd2400ec801056c709702d46c38029
KEvans254 <evanstoja@gmail.com>
2022-06-27 15:41:34 +0300

    0. Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Class Square that defines a rectangle.
+"""Class Rectangle that defines a rectangle.
     """
 
 
 class Rectangle:
-    """Class Square that defines a rectangle.
+    """Class Rectangle that defines a rectangle.
     """
     pass
